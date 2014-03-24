7f02bd2d026401bef59fcdf35a855e6296b4bbec
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-24 11:04:19 +0100

    / MO blocktags : ps_versions_compliancy added

--- blocktags.php
+++ blocktags.php
@@ -35,7 +35,7 @@ class BlockTags extends Module
 	{
 		$this->name = 'blocktags';
 		$this->tab = 'front_office_features';
-		$this->version = '1.1';
+		$this->version = '1.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -44,6 +44,7 @@ class BlockTags extends Module
 
 		$this->displayName = $this->l('Tags block');
 		$this->description = $this->l('Adds a block containing your product tags.');
+		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
 	}
 
 	function install()
