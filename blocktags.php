866231beb1fb3889b9ec8af79929048b4847b508
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-07-29 16:50:53 +0200

    // Version updated to v1.3.0

--- blocktags.php
+++ blocktags.php
@@ -33,7 +33,7 @@ class BlockTags extends Module
 	{
 		$this->name = 'blocktags';
 		$this->tab = 'front_office_features';
-		$this->version = '1.2.6';
+		$this->version = '1.3.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
